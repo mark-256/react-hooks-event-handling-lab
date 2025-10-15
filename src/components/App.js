07bd32458c920ddc1f51d2be2136519e08132586
Mark <markjohnguto@gmail.com>
2025-10-15 23:48:57 +0300

    modified

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Keypad from "./components/Keypad";
+import EyesOnMe from "./components/EyesOnMe";
+
+function App() {
+  return (
+    <div>
+      <Keypad />
+      <EyesOnMe />
+    </div>
+  );
+}
+
+export default App;
--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function handleFocus() {
+        console.log("Good!");
+    }
+
+    function handleBlur() {
+        console.log("Hey! Eyes on Me!");
+    }
+
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>
+                Eyes on me
+            </button>
+        </div>
+    );
+}
+
+export default EyesOnMe;
